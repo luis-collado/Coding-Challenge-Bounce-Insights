d9f94a2f45f01bbe5987f97b011baf40394bd68d
Luis <luis.collado.pu@gmail.com>
2023-12-01 23:27:53 +0100

    Updating Backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-
 app.use(cors());
 
 app.get('/country/:name', async (req, res) => {
